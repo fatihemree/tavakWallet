e5bf33127c2c0ffbb9186f4bacf5f2a130dcb7a6
fatihemree <fatih.emre.212@gmail.com>
2020-06-27 04:27:11 +0300

    LoginCreate AppAssetsCreate

---
+++ src/components/core/Container.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #FAFCFF;
+  justify-content: center;
+  align-items: center;
+  
+`;
+
+
+
+
+export default Container;
---
+++ src/components/core/Text.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.Text`
+    font-size: 24px;
+    font-weight: 500;
+    color: palevioletred;
+`;
+export default Title;
